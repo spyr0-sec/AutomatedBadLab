cd1f4698f7138ffc6200fd41f3e5d198f9bc7399
spyr0 <spyr0sec@protonmail.com>
2023-10-12 13:31:44 +0100

    Enforce SMB Signing on DCs

--- PostInstallationActivities/AutomatedBadLab/AD_Attack_Vectors/Disable-SMBSigning.ps1
+++ PostInstallationActivities/AutomatedBadLab/AD_Attack_Vectors/Disable-SMBSigning.ps1
@@ -1,4 +1,15 @@
 Function Disable-SMBSigning {
     Write-Host "  [+] Disabling SMB Signing.." -ForegroundColor Green
-    Set-SmbServerConfiguration -RequireSecuritySignature 0 -EnableSecuritySignature 0 -Confirm:$False
+
+    $ntdsPath = "C:\Windows\NTDS\ntds.dit"
+
+    # If DC then configure via GPO
+    If (Test-Path $ntdsPath) {
+        Set-GPPrefRegistryValue -Name "Default Domain Controllers Policy" -Key "HKLM\SYSTEM\CurrentControlSet\Services\LanManServer\Parameters" -ValueName "RequireSecuritySignature" -Type DWord -Value 0 -Context Computer -Action Update
+        Set-GPPrefRegistryValue -Name "Default Domain Controllers Policy" -Key "HKLM\SYSTEM\CurrentControlSet\Services\LanManServer\Parameters" -ValueName "EnableSecuritySignature" -Type DWord -Value 0 -Context Computer -Action Update
+        Invoke-GPUpdate -RandomDelayInMinutes 0 
+    }
+    Else {
+        Set-SmbServerConfiguration -RequireSecuritySignature 0 -EnableSecuritySignature 0 -Confirm:$False
+    }
 }
