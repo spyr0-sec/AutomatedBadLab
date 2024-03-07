ed112ec22025981d21efe3cf57d2724ff1a530af
spyr0 <spyr0sec@protonmail.com>
2024-03-07 13:02:47 +0000

    Simplify Networking

--- Labs/1. Template Standalone.ps1
+++ Labs/1. Template Standalone.ps1
@@ -1,7 +1,6 @@
 #--------------------------------------------------------------------------------------------------------------------
 # Global parameters - CHANGEME
 $LabName         = 'StandaloneTemplate'
-$Subnet          = '10.10.X.0/24'
 $AdminUser       = 'wsadmin'
 $AdminPass       = 'complexpassword'
 $MachineName     = 'WS01'
@@ -9,22 +8,12 @@ $MachineName     = 'WS01'
 # Get-LabAvailableOperatingSystem will list all available OSes to you
 $OperatingSystem = 'Windows 10 Enterprise Evaluation'
 
-# Port forward RDP access to the lab machine to make it accessible externally
-#$LPORT = "3490"
-#$RHOST = "10.10.X.3"
-
-#netsh interface portproxy add v4tov4 listenport=$LPORT listenaddress=0.0.0.0 connectport=3389 connectaddress=$RHOST
-
 #--------------------------------------------------------------------------------------------------------------------
 # LAB CREATION
 # Create our lab using HyperV (Azure is also supported)
 New-LabDefinition -Name $LabName -DefaultVirtualizationEngine HyperV
 
-# For Internal networks, just need a name and subnet space. Internet Network is a static NAT network
-Add-LabVirtualNetworkDefinition -Name $labName -AddressSpace $Subnet
-Add-LabVirtualNetworkDefinition -Name 'Internet' -AddressSpace 10.10.0.0/24 # REMOVE IF INTERNAL ONLY
-
-# Create a domain admin account to handle Windows machine creation / Active Directory configration. 
+# Create a local admin account
 Set-LabInstallationCredential -Username $AdminUser -Password $AdminPass
 
 #--------------------------------------------------------------------------------------------------------------------
@@ -39,20 +28,25 @@ $PSDefaultParameterValues = @{
 
 #--------------------------------------------------------------------------------------------------------------------
 # NETWORKING - https://automatedlab.org/en/latest/Wiki/Basic/networksandaddresses/
-# Give Workstation Internet access via NAT switch
-$WS1NICs = @()
-$WS1NICs += New-LabNetworkAdapterDefinition -VirtualSwitch $LabName
-$WS1NICs += New-LabNetworkAdapterDefinition -VirtualSwitch 'Internet' -UseDhcp # REMOVE IF INTERNAL ONLY
+$VSwitch = Get-VMSwitch | Where-Object SwitchType -eq 'External'
+Add-LabVirtualNetworkDefinition -Name $VSwitch.Name -HyperVProperties @{
+    SwitchType = $VSwitch.SwitchType
+    AdapterName = $VSwitch.NetAdapterInterfaceDescription
+}
 
 #--------------------------------------------------------------------------------------------------------------------
 # MACHINE CREATION - https://automatedlab.org/en/latest/Wiki/Basic/addmachines/
-
-# For the workstation, use Get-LabAvailableOperatingSystem to get correct OS name
-$VSRole = Get-LabPostInstallationActivity -CustomRole VisualStudio2022
-Add-LabMachineDefinition -Name $MachineName -NetworkAdapter $WS1NICs -OperatingSystem $OperatingSystem -PostInstallationActivity $VSRole
+Add-LabMachineDefinition -Name $MachineName -Network $VSwitch.Name -OperatingSystem $OperatingSystem
 
 # Install our lab, has flags for level of output
 Install-Lab # -Verbose -Debug
+
+# UX+++ (https://twitter.com/awakecoding/status/1750736577746084162)
+Invoke-LabCommand -ComputerName (Get-LabVM) -ActivityName "RemoveFirstRunExperience" -ScriptBlock {
+    New-Item -Path "HKLM:\Software\Policies\Microsoft\Edge" -Force | Out-Null
+    Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Edge" -Name "HideFirstRunExperience" -Value 1
+    Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Edge" -Name "NewTabPageLocation" -Value "https://google.com"
+}
 
 # Provides a pretty table detailing all elements of what has been created
 Show-LabDeploymentSummary -Detailed
