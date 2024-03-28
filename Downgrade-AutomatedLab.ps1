fd3b4fcb1a1d93a19b92089de43eb771220dddba
spyr0 <spyr0sec@protonmail.com>
2024-03-28 23:47:07 +0000

    Add helper script

---
+++ Downgrade-AutomatedLab.ps1
@@ -0,0 +1,19 @@
+# Helper script to downgrade AutomatedLab to a specific version. Run in a fresh PwSh session
+# v5.51.0 has an issue with network creation / deletion
+# https://github.com/AutomatedLab/AutomatedLab/issues/1391
+# AutomatedLab.Common is still stuck on v2.3.25 so excluded from downgrade
+
+Get-Module AutomatedLab* | Remove-Module -Verbose
+
+$latestAutomatedLabVersion = (Find-Module AutomatedLab).Version
+$requiredAutomatedLabVersion = "5.50.0"
+Install-Module -Name AutomatedLab -RequiredVersion $requiredAutomatedLabVersion -Force -SkipPublisherCheck -AllowClobber
+If ( $requiredAutomatedLabVersion -ne $latestAutomatedLabVersion ) {
+    "AutomatedLabDefinition", "AutomatedLabNotifications", "AutomatedLabTest", "AutomatedLabWorker", "AutomatedLabUnattended", "AutomatedLab.Ships", "AutomatedLab.Recipe", "AutomatedLabCore", "AutomatedLab" | ForEach-Object {
+        Install-Module -Name $_ -RequiredVersion $requiredAutomatedLabVersion -Verbose -Force
+        Uninstall-Module -Name $_ -RequiredVersion $latestAutomatedLabVersion -Verbose -Force
+    }
+}
+
+Import-Module AutomatedLab -Force
+Get-Module AutomatedLab*
